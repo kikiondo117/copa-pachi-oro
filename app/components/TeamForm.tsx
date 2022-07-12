3027ca1e16741db43eb6d8ebf6f45b2389d5c83e
Carlos Vera <kikiondo117@gmail.com>
2022-07-11 21:42:26 -0500

    OVER-landing-000 Form styles

--- app/components/TeamForm.tsx
+++ app/components/TeamForm.tsx
@@ -1,18 +1,18 @@
 export function TeamForm() {
   return (
-    <form className="flex flex-col bg-special-gray px-4">
-      <p>Crea tu cuenta</p>
+    <form className="flex flex-col bg-special-gray px-4 ">
+      <p className=" mt-6">Crea tu cuenta</p>
       <input className="mt-2" type="email" />
       <input className="mt-4" type="password" />
       <input className="mt-4" type="password" />
       <p className="mt-2">Datos del equipo</p>
       <input className="mt-4" type="text" />
-      <div className="flex justify-between">
+      <div className="flex justify-between mt-4">
         <select className="w-full" name="region" id="region"></select>
         <select className="w-full" name="plataforma" id="plataforma"></select>
       </div>
       <input className="mt-4" type="img" />
-      <button>Registrar equipo</button>
+      <button className=" my-9">Registrar equipo</button>
     </form>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,12 +5,12 @@ export default function Index() {
   return (
     <div className="min-h-screen">
       <Header />
-      <main className="bg-hero-tracer h-screen mx-auto flex items-center">
-        <div className="container m-auto grid grid-cols-12 pt-14">
-          <h2 className="col-start-1 col-end-7 text-white text-7xl md:text-5xl">
+      <main className="bg-hero-tracer min-h-screen mx-auto flex items-center pt-14">
+        <div className="container m-auto grid grid-cols-12">
+          <h2 className="col-start-1 col-end-7 text-white text-7xl ">
             La comunidad te necesita, únete ahora y compite
           </h2>
-          <div className="col-start-8 col-end-12">
+          <div className="col-start-8 col-end-12 max-w-sm">
             <div className="bg-form-top h-24 w-auto" />
             <TeamForm />
           </div>
